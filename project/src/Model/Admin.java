6126b3f4cca74ba123ff2f3f094e6b9386eacb9a
YehiaGazzarine <yahya.gazzarine.mg@gmail.com>
2025-10-26 16:51:41 +0300

    Slight change made

--- project/src/Model/Admin.java
+++ project/src/Model/Admin.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Admin {
     private final StudentDatabase dataBase;
 
-    Admin() {
+    public Admin() {
         dataBase = new StudentDatabase("StudentsDatabase.txt");
         dataBase.readFromFile();
     }
